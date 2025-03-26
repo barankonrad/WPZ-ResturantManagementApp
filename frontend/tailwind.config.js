a91ee42d27590df9e562ff87e5b47272e438278f
Wojciech Sierotowicz <wojexe@gmail.com>
2025-03-26 20:14:23 +0100

    Downgrade to Tailwind 3

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {}
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")]
+};
