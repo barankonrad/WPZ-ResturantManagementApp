a9cbc0fd9a82bb5d16989a76a1061f56c95abd68
Tymec <tymek1rt@hotmail.com>
2025-03-26 22:01:25 +0100

    Add navbar to layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
   import "../app.css";
+  import { ModeWatcher } from "mode-watcher";
+  import Navbar from "$lib/components/navbar.svelte";
 
   let { children } = $props();
+  let user = false;
 </script>
 
-{@render children()}
+<ModeWatcher />
+<div class="flex min-h-screen flex-col">
+  <header class="bg-gray-100 dark:bg-gray-900 shadow-md">
+    <Navbar {user}></Navbar>
+  </header>
+
+  {@render children()}
+
+  <footer class="bg-gray-100 dark:bg-gray-900 shadow-md"></footer>
+</div>
