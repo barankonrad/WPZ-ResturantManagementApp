59c10429aeaf0264750ffaf478d03d7f6aeca364
Wojciech Sierotowicz <wojexe@gmail.com>
2025-03-27 23:44:47 +0100

    both forms done

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -4,4 +4,6 @@
   let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex min-h-screen h-full place-content-center place-items-center">
+  {@render children()}
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { buttonVariants } from "$lib/components/ui/button";
+  import { Card, Content, Header, Title } from "$lib/components/ui/card";
+</script>
+
+<Card class="w-96">
+  <Header>
+    <Title>Who are you?</Title>
+  </Header>
+  <Content>
+    <a href="/login" class={buttonVariants({ variant: "outline" })}>Login</a>
+    <a href="/register" class={buttonVariants({ variant: "default" })}>Register</a>
+  </Content>
+</Card>
