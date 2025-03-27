b61d4c850a4a1c3a41e5edc7ef2ae4f3287ed551
Tymec <tymek1rt@hotmail.com>
2025-03-27 21:55:44 +0100

    Add id to user data

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -2,6 +2,7 @@ import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
   event.locals.user = {
+    id: "3007388d-e0cb-468f-b5da-d086d3e3ac84",
     email: "admin@example.org",
     role: "admin"
   };
--- frontend/src/lib/$types.d.ts
+++ frontend/src/lib/$types.d.ts
@@ -3,6 +3,7 @@
 export type UserRole = "admin" | "manager" | "chef" | "waiter" | "user";
 
 export type UserData = {
+  id: string;
   email: string;
   role: UserRole;
 };
