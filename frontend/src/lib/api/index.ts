f2d4b1dd5f18851fc162479a888b3dc059318869
Wojciech Sierotowicz <wojexe@gmail.com>
2025-04-26 13:17:21 +0200

    [WPZ-17] Improve authentication flow/code (#19)
    * Improve authentication flow/code
    
    * Login works wihtout proxy
    
    * Change logout to use POST

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,1 +1,3 @@
-export const baseURL = "https://ordersync.online";
+import { PUBLIC_API_BASE_URL } from "$env/static/public";
+
+export const baseURL = PUBLIC_API_BASE_URL;
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,16 +1,79 @@
-<script>
-  import { buttonVariants } from "$lib/components/ui/button";
+<script lang="ts">
   import { Card, Content, Header, Title } from "$lib/components/ui/card";
+
+  import * as Form from "$lib/components/ui/form";
+  import { Input } from "$lib/components/ui/input";
+
+  import { superForm, setError, defaults } from "sveltekit-superforms";
+  import { login, LoginRequestSchema } from "$lib/api/auth";
+  import { valibot } from "sveltekit-superforms/adapters";
+  import { goto } from "$app/navigation";
+
+  const form = superForm(defaults(valibot(LoginRequestSchema)), {
+    SPA: true,
+    validators: valibot(LoginRequestSchema),
+    onUpdate: async ({ form }) => {
+      if (!form.valid) {
+        return;
+      }
+
+      const { authenticated, error } = await login(form.data);
+
+      if (authenticated) {
+        goto("/menu");
+      } else {
+        switch (typeof error) {
+          case "string":
+            setError(form, error);
+            break;
+          case "object":
+            setError(form, (error as any)?.message ?? "Authentication failed");
+            break;
+          default:
+            setError(form, JSON.stringify(error));
+        }
+      }
+    }
+  });
+
+  const { form: formData, errors, enhance } = form;
 </script>
 
 <div class="flex h-full w-full flex-grow items-center justify-center">
   <Card class="w-96">
     <Header>
-      <Title>Who are you?</Title>
+      <Title>Login</Title>
     </Header>
     <Content>
-      <a href="/login" class={buttonVariants({ variant: "outline" })}>Login</a>
-      <a href="/register" class={buttonVariants({ variant: "default" })}>Register</a>
+      <form method="POST" use:enhance class="flex flex-col">
+        <Form.Field {form} name="email">
+          <Form.Control>
+            {#snippet children({ props })}
+              <Form.Label>Email</Form.Label>
+              <Input {...props} type="email" bind:value={$formData.email} />
+            {/snippet}
+          </Form.Control>
+          <Form.FieldErrors />
+        </Form.Field>
+
+        <Form.Field {form} name="password">
+          <Form.Control>
+            {#snippet children({ props })}
+              <Form.Label>Password</Form.Label>
+              <Input {...props} type="password" bind:value={$formData.password} />
+            {/snippet}
+          </Form.Control>
+          <Form.FieldErrors />
+        </Form.Field>
+
+        {#if $errors._errors}
+          <div class="mt-3 w-full text-center text-red-500">
+            {$errors._errors!.join(",")}
+          </div>
+        {/if}
+
+        <Form.Button class="mt-3">Submit</Form.Button>
+      </form>
     </Content>
   </Card>
 </div>
