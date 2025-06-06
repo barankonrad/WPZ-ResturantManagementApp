dd9e601ca789e415d6a5366f146f510f394f71e7
Wojciech Sierotowicz <wojexe@gmail.com>
2025-06-06 22:47:43 +0200

    private env

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,5 +1,3 @@
 import { env } from "$env/dynamic/public";
-import { env as privateEnv } from "$env/dynamic/private"
 
 export const baseURL = env.PUBLIC_API_BASE_URL;
-export const internalBaseURL = privateEnv.API_BASE_URL;
