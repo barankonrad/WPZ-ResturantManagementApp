7d74fd8e505cf283e3329fc5f13dec74d2ed925f
Piotr-Lenarczyk <piotr.lenarczyk00@gmail.com>
2025-06-11 15:00:25 +0200

    Validate update request for null/empty value

--- backend/src/main/java/org/example/restaurantmanagementapplication/model/in/OrderUpdateRequest.java
+++ backend/src/main/java/org/example/restaurantmanagementapplication/model/in/OrderUpdateRequest.java
@@ -1,10 +1,12 @@
 package org.example.restaurantmanagementapplication.model.in;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class OrderUpdateRequest {
+	@NotEmpty
 	private List<OrderItemUpdateRequest> orderedItems;
 }
